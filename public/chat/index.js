355550effcebee390d8ee37d6b5b369a02c2e38e
David Krause <davidkrause@Davids-MBP.lan1>
2018-12-06 16:59:17 -0600

    Add chat code Change README.md

--- public/chat/index.js
+++ public/chat/index.js
@@ -1,6 +1,5 @@
-$(function() {
-  // Get handle to the chat div
-  var $chatWindow = $('#messages');
+$ (function () {
+  var $chatWindow = $ ('#messages');
 
   // Our interface to the Chat service
   var chatClient;
@@ -14,98 +13,143 @@ $(function() {
   var username;
 
   // Helper function to print info messages to the chat window
-  function print(infoMessage, asHtml) {
-    var $msg = $('<div class="info">');
+  function print (infoMessage, asHtml) {
+    var $msg = $ ('<div class="info">');
     if (asHtml) {
-      $msg.html(infoMessage);
+      $msg.html (infoMessage);
     } else {
-      $msg.text(infoMessage);
+      $msg.text (infoMessage);
     }
-    $chatWindow.append($msg);
+    $chatWindow.append ($msg);
   }
 
   // Helper function to print chat message to the chat window
-  function printMessage(fromUser, message) {
-    var $user = $('<span class="username">').text(fromUser + ':');
+  function printMessage (fromUser, message) {
+    var $user = $ ('<span class="username">').text (fromUser + ':');
     if (fromUser === username) {
-      $user.addClass('me');
+      $user.addClass ('me');
     }
-    var $message = $('<span class="message">').text(message);
-    var $container = $('<div class="message-container">');
-    $container.append($user).append($message);
-    $chatWindow.append($container);
-    $chatWindow.scrollTop($chatWindow[0].scrollHeight);
+    var $message = $ ('<span class="message">').text (message);
+    var $container = $ ('<div class="message-container">');
+    $container.append ($user).append ($message);
+    $chatWindow.append ($container);
+    $chatWindow.scrollTop ($chatWindow[0].scrollHeight);
   }
 
   // Alert the user they have been assigned a random username
-  print('Logging in...');
+  print ('Logging in...');
 
   // Get an access token for the current user, passing a username (identity)
   // and a device ID - for browser-based apps, we'll always just use the
   // value "browser"
-  $.getJSON('/token', {
-    device: 'browser'
-  }, function(data) {
-    // Alert the user they have been assigned a random username
-    username = data.identity;
-    print('You have been assigned a random username of: '
-    + '<span class="me">' + username + '</span>', true);
+  $.getJSON (
+    '/token',
+    {
+      device: 'browser',
+    },
+    function (data) {
+      // Alert the user they have been assigned a random username
+      username = data.identity;
+      print (
+        'You have been assigned a random username of: ' +
+          '<span class="me">' +
+          username +
+          '</span>',
+        true
+      );
 
-    // Initialize the Chat client
-    Twilio.Chat.Client.create(data.token).then(client => {
-      chatClient = client;
-      chatClient.getSubscribedChannels().then(createOrJoinGeneralChannel);
-    });
-  });
+      // Initialize the Chat client
+      Twilio.Chat.Client.create (data.token).then (client => {
+        chatClient = client;
+        chatClient.getSubscribedChannels ().then (createOrJoinGeneralChannel);
+      });
+    }
+  );
 
-  function createOrJoinGeneralChannel() {
+  function createOrJoinGeneralChannel () {
     // Get the general chat channel, which is where all the messages are
     // sent in this simple application
-    print('Attempting to join "general" chat channel...');
-    chatClient.getChannelByUniqueName('general')
-    .then(function(channel) {
-      generalChannel = channel;
-      console.log('Found general channel:');
-      console.log(generalChannel);
-      setupChannel();
-    }).catch(function() {
-      // If it doesn't exist, let's create it
-      console.log('Creating general channel');
-      chatClient.createChannel({
-        uniqueName: 'general',
-        friendlyName: 'General Chat Channel'
-      }).then(function(channel) {
-        console.log('Created general channel:');
-        console.log(channel);
+    print ('Attempting to join "general" chat channel...');
+    chatClient
+      .getChannelByUniqueName ('general')
+      .then (function (channel) {
         generalChannel = channel;
-        setupChannel();
-      }).catch(function(channel) {
-        console.log('Channel could not be created:');
-        console.log(channel);
+        console.log ('Found general channel:');
+        console.log (generalChannel);
+        setupChannel ();
+      })
+      .catch (function () {
+        // If it doesn't exist, let's create it
+        console.log ('Creating general channel');
+        chatClient
+          .createChannel ({
+            uniqueName: 'general',
+            friendlyName: 'General Chat Channel',
+          })
+          .then (function (channel) {
+            console.log ('Created general channel:');
+            console.log (channel);
+            generalChannel = channel;
+            setupChannel ();
+          })
+          .catch (function (channel) {
+            console.log ('Channel could not be created:');
+            console.log (channel);
+          });
       });
-    });
   }
 
   // Set up channel after it has been found
-  function setupChannel() {
+  function setupChannel () {
     // Join the general channel
-    generalChannel.join().then(function(channel) {
-      print('Joined channel as '
-      + '<span class="me">' + username + '</span>.', true);
+    generalChannel.join ().then (function (channel) {
+      print (
+        'Joined channel as ' + '<span class="me">' + username + '</span>.',
+        true
+      );
     });
 
     // Listen for new messages sent to the channel
-    generalChannel.on('messageAdded', function(message) {
-      printMessage(message.author, message.body);
+    generalChannel.on ('messageAdded', function (message) {
+      printMessage (message.author, message.body);
     });
   }
 
   // Send a new message to the general channel
-  var $input = $('#chat-input');
-  $input.on('keydown', function(e) {
+  var $input = $ ('#chat-input');
+  $input.on ('keydown', function (e) {
     if (e.keyCode == 13) {
-      generalChannel.sendMessage($input.val())
-      $input.val('');
+      generalChannel.sendMessage ($input.val());
+      const url =
+        'https://api.twilio.com/2010-04-01/Accounts/' + process.env.TWILIO_ACCOUNT_SID + '/Messages.json';
+      const details = {
+        Body: $input.val(),
+        From: '+14322192238',
+        To: '+14322882200',
+      };
+
+      var formBody = [];
+      for (var property in details) {
+        var encodedKey = encodeURIComponent (property);
+        var encodedValue = encodeURIComponent (details[property]);
+        formBody.push (encodedKey + '=' + encodedValue);
+      }
+      formBody = formBody.join ('&');
+
+      fetch (url, {
+        method: 'POST', // or 'PUT'
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          Authorization: 'Basic QUNlMjY0MmNjYTdhZTgwM2VmYmJhNzU4N2I0ZTEzNzk4OTpjYWUxODEyZDVkMzRjNDI0YmRmYTM4ZmNiNWNmZDEyOQ==',
+        },
+        body: formBody,
+      })
+        .then (response => response.json ())
+        .then (message => console.log (message))
+        .catch (e => console.error (e));
+
+
+      $input.val ('');
     }
   });
 });
